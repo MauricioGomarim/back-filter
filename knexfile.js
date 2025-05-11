28d0c64228e100316abaf072fbb2b95ef9f02584
Mauricio Gomarim <mauricio.gomarim3@gmail.com>
2025-05-11 17:51:12 -0300

    att banco

--- knexfile.js
+++ knexfile.js
@@ -1,29 +1,29 @@
-// require('dotenv').config();
-
-// module.exports = {
-//   development: {
-//     client: 'pg',
-//     connection: {
-//       connectionString: process.env.DATABASE_URL,
-//       ssl: { rejectUnauthorized: false }
-//     },
-//     migrations: {
-//       tableName: 'knex_migrations'
-//     }
-//   }
-// };
-
+require('dotenv').config();
 
 module.exports = {
   development: {
-    client: 'sqlite3',
+    client: 'pg',
     connection: {
-      filename: './src/database/database.db'
+      connectionString: process.env.DATABASE_URL,
+      ssl: { rejectUnauthorized: false }
     },
-    useNullAsDefault: true,
     migrations: {
-      tableName: 'knex_migrations',
-      directory: './migrations' // ajuste se suas migrations estiverem em outro lugar
+      tableName: 'knex_migrations'
     }
   }
 };
+
+
+// module.exports = {
+//   development: {
+//     client: 'sqlite3',
+//     connection: {
+//       filename: './src/database/database.db'
+//     },
+//     useNullAsDefault: true,
+//     migrations: {
+//       tableName: 'knex_migrations',
+//       directory: './migrations' // ajuste se suas migrations estiverem em outro lugar
+//     }
+//   }
+// };
